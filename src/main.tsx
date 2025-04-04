2cb0a2fa89a27caf78863a41671893e0eda2d9d2
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-04-04 21:07:08 +0300

    feat: application is wrapped by the provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/app/App'
 import '@fontsource/roboto/300.css'
@@ -6,9 +5,11 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import '@/app/styles/index.scss'
+import { Provider } from 'react-redux'
+import { store } from '@/app/store'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 )
