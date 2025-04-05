6fb06f49c8a4195e2886127d62ab9bf401d97fe9
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-04-05 09:59:48 +0300

    feat: implemented App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,32 @@
+import { Router } from '@/common/routes/Router'
+import { toast, ToastContainer } from 'react-toastify'
+import { useSelector } from 'react-redux'
+import { selectAppError } from '@/app/appSelectors'
+import { useEffect } from 'react'
+
 export function App() {
-  return <div>Hello</div>
+  const error = useSelector(selectAppError)
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error)
+    }
+  }, [error])
+
+  return (
+    <div>
+      <ToastContainer
+        autoClose={3000}
+        closeOnClick
+        draggable
+        hideProgressBar={false}
+        newestOnTop={false}
+        pauseOnFocusLoss
+        pauseOnHover
+        position={'bottom-left'}
+        rtl={false}
+      />
+      <Router />
+    </div>
+  )
 }
